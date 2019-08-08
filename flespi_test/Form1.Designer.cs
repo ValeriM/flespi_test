7bc38223341733d81c6d011377070272e0fab3cb
Valeriy Makarov <valerim@yandex.ru>
2019-08-08 22:46:09 +0500

    Add project files.

---
+++ flespi_test/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace flespi_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTopicSubscribe = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtReceived = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTopicPublish = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Topic";
+            // 
+            // txtTopicSubscribe
+            // 
+            this.txtTopicSubscribe.Location = new System.Drawing.Point(106, 5);
+            this.txtTopicSubscribe.Name = "txtTopicSubscribe";
+            this.txtTopicSubscribe.Size = new System.Drawing.Size(100, 20);
+            this.txtTopicSubscribe.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Subscribe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtReceived
+            // 
+            this.txtReceived.Location = new System.Drawing.Point(106, 63);
+            this.txtReceived.Name = "txtReceived";
+            this.txtReceived.Size = new System.Drawing.Size(100, 20);
+            this.txtReceived.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Received";
+            // 
+            // txtTopicPublish
+            // 
+            this.txtTopicPublish.Location = new System.Drawing.Point(106, 117);
+            this.txtTopicPublish.Name = "txtTopicPublish";
+            this.txtTopicPublish.Size = new System.Drawing.Size(100, 20);
+            this.txtTopicPublish.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Topic Publish";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Publish";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtTopicPublish);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtReceived);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTopicSubscribe);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTopicSubscribe;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtReceived;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTopicPublish;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ flespi_test/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace flespi_test
+{
+    public partial class Form1 : Form
+    {
+        MqttClient client;
+        string clientId;
+        static readonly string token = "Vo6wSNjDEM19qzUdq9qbwZugZPmPl3N4hHq0lAtPalMqIwuYuKZQxiUnX7060B17";
+
+        public Form1()
+        {
+            InitializeComponent();
+            string BrokerAddress = "test.mosquitto.org";
+            BrokerAddress = "mqtt.flespi.io";
+            client = new MqttClient(BrokerAddress);
+
+            // register a callback-function (we have to implement, see below) which is called by the library when a message was received
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // use a unique id as client id, each time we start the application
+            clientId = Guid.NewGuid().ToString();
+
+            client.Connect(clientId, token, "");
+
+            Text = client.IsConnected ? "Connected" : "Хуй там";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (client.IsConnected)
+                client.Disconnect();
+
+            base.OnClosed(e);
+            //App.Current.Shutdown();
+            Application.Exit();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtTopicSubscribe.Text != "")
+            {
+                // whole topic
+                string Topic = txtTopicSubscribe.Text;
+
+                // subscribe to the topic with QoS 2
+                ushort code = client.Subscribe(new string[] { Topic }, new byte[] { 2 });   // we need arrays as parameters because we can subscribe to different topics with one call
+                txtReceived.Text = code.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You have to enter a topic to subscribe!");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (txtTopicPublish.Text != "")
+            {
+                // whole topic
+                string Topic = txtTopicPublish.Text;
+
+                // publish a message with QoS 2
+                ushort code = client.Publish(Topic, Encoding.UTF8.GetBytes(txtTopicPublish.Text), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+                txtReceived.Text = code.ToString();
+            }
+            else
+            {
+                MessageBox.Show("You have to enter a topic to publish!");
+            }
+        }
+        // this code runs when a message was received
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            MessageBox.Show(ReceivedMessage);
+
+            //Dispatcher.Invoke(delegate {              // we need this construction because the receiving code in the library and the UI with textbox run on different threads
+            //    txtReceived.Text = ReceivedMessage;
+            //});
+        }
+    }
+}
